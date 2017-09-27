31ff7b89209d19e20c4defd31bd316105f0f7425
jeyerena <jeduardoyerena@gmail.com>
2017-09-27 15:08:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 echo "<h1> Test Page </h1>";
-echo "<img src='ClassTeamBuilder/src/Interface/'Groupre Icon.jpg'' alt= 'Groupre'>";
+echo "<img src='ClassTeamBuilder/src/interface/'Groupre Icon.jpg'' alt= 'Groupre'>";
