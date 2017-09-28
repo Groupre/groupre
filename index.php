a9570e9ea63820227a188d9834d2e0fe5162a2c7
jeyerena <jeduardoyerena@gmail.com>
2017-09-27 23:40:27 -0400

    testing again

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-echo "<h1> Test Page </h1>";
-echo "<img src='src/interface/groupre.jpg' alt= 'Groupre'>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel = "stylesheet" type = "text/css" href = "src/interface/test.css">
+    <title>Groupre: the Class Team Builder</title>
+</head>
+<body>
+<h1>Groupre</h1>
+<img class="fish" alt="groupythegrouper" src='src/interface/groupre.jpg' alt= 'Groupre'>
+<br>
+<a href="https://github.com/jeyerena/ClassTeamBuilder"><img alt="Github page" src="src/interface/GitHub-Mark/PNG/GitHub-Mark-64px.png"></a>
+</body>
+</html>
