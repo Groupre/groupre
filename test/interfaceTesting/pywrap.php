0026879c976c2a18b05a7b1efe28b7d23b9a8193
billsunday66 <31862952+billsunday66@users.noreply.github.com>
2017-10-02 11:43:36 -0400

    Add files via upload

---
+++ test/interfaceTesting/pywrap.php
@@ -0,0 +1,11 @@
+<?php
+
+$command = escapeshellcmd('https://cdn.rawgit.com/jeyerena/ClassTeamBuilder/tree/master/test/interfaceTesting/groupre.py');
+$output = shell_exec($command);
+echo $output;
+echo 'Thank you for using Groupre! <br />';
+
+
+//https://cdn.rawgit.com/jeyerena/ClassTeamBuilder/tree/master/test/interfaceTesting/
+
+?>
