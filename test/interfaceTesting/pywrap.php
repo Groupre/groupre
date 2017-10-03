027b87e4f3a7b786d2f616de007e725c3f9153b3
billsunday66 <31862952+billsunday66@users.noreply.github.com>
2017-10-03 12:23:41 -0400

    Update pywrap.php

--- test/interfaceTesting/pywrap.php
+++ test/interfaceTesting/pywrap.php
@@ -1,6 +1,6 @@
 <?php
 
-$command = escapeshellcmd('https://rawgit.com/jeyerena/ClassTeamBuilder/tree/master/test/interfaceTesting/groupre.py');
+$command = escapeshellcmd('groupre.py');
 $output = shell_exec($command);
 echo $output;
 echo 'Thank you for using Groupre! <br />';
