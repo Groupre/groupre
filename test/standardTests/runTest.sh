ab90ace4ee750288939fa656a0369a19147a91fc
Kyle Dixon <mrkylezd@gmail.com>
2017-10-12 01:38:46 -0400

    Made runTest.sh output to stdout file.

--- test/standardTests/runTest.sh
+++ test/standardTests/runTest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ../../src/python/groupre.py -c ../testFiles/chairs/chairsTest.csv -s ../testFiles/students/studentsTest.csv -o testOutput.csv
+python ../../src/python/groupre.py -c ../testFiles/chairs/chairsTest.csv -s ../testFiles/students/studentsTest.csv -o testOutput.csv > log
