021a5b8c8f6e181873c7be792c8c85ae758e09af
Kyle Dixon <mrkylezd@gmail.com>
2017-10-26 23:22:18 -0400

    Changed using scripts for root py files to using the root "package".

--- src/groupre/setup.py
+++ src/groupre/setup.py
@@ -10,12 +10,8 @@ setup(
     description='''Matching tool for assigning students to chairs
         based on student preferences and chair attributes.''',
     url='https://github.com/jeyerena/ClassTeamBuilder/tree/master',
-    packages=['data_structures', 'helpers', 'matching_algorithms'],
+    packages=['', 'data_structures', 'helpers', 'matching_algorithms'],
     # install_requires=[]
-    scripts=[
-        'groupre.py',
-        'groupre_globals.py'
-    ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha'
     ],
