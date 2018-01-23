55e45088b4128533e14e51ad35befd857d958815
kzdixon <mrkylezd@gmail.com>
2018-01-23 15:36:16 -0500

    Comment added, minor tweak to docstring.

--- src/groupre/matching_algorithms/gender_random_match.py
+++ src/groupre/matching_algorithms/gender_random_match.py
@@ -7,7 +7,7 @@ from data_structures import TeamMember
 
 def gender_random_match(student, chairs, team_fields, team_structures):
     '''This method will find a team for a given student based on their
-    gender attribute..'''
+    gender attribute.'''
 
     # This code assumes that teams are of sizes greater than at least 4.
 
@@ -94,6 +94,10 @@ def gender_random_match(student, chairs, team_fields, team_structures):
             # print('No available unoccupied chair located in this team.')
 
         num_checked += 1
+
+    # The below section seems to be a sanity check as well as a removal of
+    # the chair from our primary list. Can likely be simplified and we can
+    # maybe remove the sanity check
 
     found_chair = False
     # print('using chair: ' + chair.chair_id)
