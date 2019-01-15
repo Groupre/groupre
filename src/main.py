f26b287c61b42b96f0a0ead3bab30b66dc60efef
kamisch <chengtwv2@gmail.com>
2019-01-15 17:21:18 -0500

    yo

--- src/main.py
+++ src/main.py
@@ -1,81 +1,117 @@
 import csv
 from data_structures.student import Student
 from data_structures.chair import Chair
-
-
-def sortStudentFunc(student_list):
-    student_pool = [[], [], [], [], [], [], []]
-
-    for student in student_list:
-        print(student.is_VIP)
-        if student.has_pref:
-            pref_count = -1
-            if not student.is_VIP:
-                pref_count += 3
-            if student.pref_front:
-                pref_count += 1
-            if student.pref_back:
-                pref_count += 1
-            if student.pref_aisle:
-                pref_count += 1
-            if student.pref_left:
-                pref_count += 1
-            student_pool[pref_count].append(student)
-        else:
-            student_pool[6].append(student)
-
-    return student_pool
-
-
-def insertionSort(arr):
-    for i in range(1, len(arr)):
-        key = arr[i]
-        j = i-1
-        while j >= 0 and key.chair_id < arr[j].chair_id:
-                arr[j+1] = arr[j]
-                j -= 1
-        arr[j+1] = key
-
-
-def sort_group(group_list):
-    #Sort the groups on number of preferences (most to least)
-    return group_list
-
-
+ 
+ 
+# def sortStudentFunc(student_list):
+#     student_pool = [[], [], [], [], [], [], []]
+ 
+#     for student in student_list:
+#         print(student.is_VIP)
+#         if student.has_pref:
+#             if student.num_pref == 1:
+#                 pref_count = 6
+#                 if student.is_VIP:
+#                     pref_count -= 2
+#                 if student.pref_front:
+#                     pref_count -= 3
+#                 if student.pref_back:
+#                     pref_count -= 2
+#                 if student.pref_aisle:
+#                     pref_count -= 2
+#                 if student.pref_left:
+#                     pref_count -= 1
+#                 student_pool[pref_count].append(student)
+#             elif student.num_pref == 2:
+#                 student_pool[0].append(student)
+#             elif student.num_pref == 3:
+#                 student_pool[0].insert(student,0)
+#         else:
+#             student_pool[6].append(student)
+ 
+#     return student_pool
+ 
+ 
+# def insertionSort(arr):
+#     for i in range(1, len(arr)):
+#         key = arr[i]
+#         j = i-1
+#         while j >= 0 and key.chair_id < arr[j].chair_id:
+#                 arr[j+1] = arr[j]
+#                 j -= 1
+#         arr[j+1] = key
+ 
+ 
+# def sort_group(group_list):
+#     #Sort the groups on number of preferences (most to least)
+#     return group_list
+ 
+ 
 def placeStudents(student_list, chair_list):
-    for chair in chair_list:
-        if chair.is_broken:
-            continue
-        top = 0
-        top_student = student_list[0]
+    # for chair in chair_list:
+    #     if chair.is_broken:
+    #         continue
+    #     top = 0
+    #     top_student = student_list[0]
+    #     for student in student_list:
+    #         print(student)
+    #         this = 0
+    #         if chair.front and student.pref_front:
+    #             this += 10
+    #         elif chair.back and student.pref_back:
+    #             this += 7
+    #         if chair.aisle and student.pref_aisle:
+    #             this += 5
+    #         if chair.left and student.pref_left:
+    #             this += 4
+    #         if this > top:
+    #             top = this
+    #             top_student = student
+    #     chair.student_id = top_student.student_id
+    #     del student_list[student_list.index(top_student)]
+    # print("it works")
+    pairs = []
+    for chair in chair_list:  
         for student in student_list:
-            print(student)
-            this = 0
-            if chair.front and student.pref_front:
-                this += 10
-            elif chair.back and student.pref_back:
-                this += 10
-            if chair.aisle and student.pref_aisle:
-                this += 10
-            if chair.left and student.pref_left:
-                this += 1
-            if this > top:
-                top = this
-                top_student = student
-        chair.student_id = top_student.student_id
-        del student_list[student_list.index(top_student)]
-    print("it works")
-    return student_list
-
-
+            if not chair.is_broken and not chair.taken:
+                if not student.taken and student.num_points > 99:
+                    student.num_points -= 100
+                    if chair.num_points == student.num_points:
+                        pairs.append([chair.chair_id,student.student_id])
+                        chair.taken = True
+                        student.taken = True
+     
+    for chair in chair_list:
+        for student in student_list:
+            if not chair.is_broken and not chair.taken:
+                if not student.taken:
+                    if chair.num_points == student.num_points:
+                        pairs.append([chair.chair_id,student.student_id])
+                        chair.taken = True
+                        student.taken = True
+    for chair in chair_list:
+        for student in student_list:
+            if not chair.is_broken and not chair.taken:
+                if not student.taken:
+                    pairs.append([chair.chair_id,student.student_id])
+                    chair.taken = True
+                    student.taken = True
+ 
+    # for chair in chair_list:
+    #     for student in student_list:
+    #         if not chair.is_broken and not chair.taken:
+    #             if not student
+    return pairs
+ 
+ 
 if __name__ == '__main__':
-    student_file = '../test/randomizedTests/students/test_students_demo_100.csv'
+    student_file = '../test/randomizedTests/students/test_students_1.csv'
     chair_file = '../test/newTests/room.csv'
     student_count = sum(1 for line in open(student_file))-1
     chair_count = sum(1 for line in open(chair_file))-1
     student_list = []
     chair_list = []
-
+ 
     with open(student_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         first_line = True
@@ -87,33 +123,42 @@ if __name__ == '__main__':
                     student_list.append(Student(row[0],row[2],[]))
                 else:
                     student_list.append(Student(row[0], row[2], row[4:]))
-
+ 
     with open(chair_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         first_line = True
         for row in csv_reader:
+            print(row)
             if first_line:
                 first_line = False
             else:
                 if len(row) <= 2:
-                    chair_list.append(Chair(row[0],""))
+                    chair_list.append(Chair(row[0],[]))
                 else:
                     chair_list.append(Chair(row[0], row[2:]))
-    sorted_student_list = sortStudentFunc(student_list)
-
+    # sorted_student_list = sortStudentFunc(student_list)
+ 
     #
-    insertionSort(chair_list)
-
-    for chair in chair_list:
-        print(chair)
-    print("printing students")
-    newStudents = []
-    for student in sorted_student_list:
-        for n in student:
-            newStudents.append(n)
-            print(n)
-    print("end printing")       
-    placeStudents(newStudents, chair_list)
-
-    for chair in chair_list:
-        print(chair)
+    # insertionSort(chair_list)
+ 
+    # for chair in chair_list:
+    #     print(chair)
+    # print("printing students")
+    # newStudents = []
+    # for student in sorted_student_list:
+    #     for n in student:
+    #         newStudents.append(n)
+    #         print(n)
+    # print("end printing")  
+          
+    for i in student_list:
+        print(i.num_points)
+    print("printing chair")
+    for n in chair_list:
+        print(n.num_points)
+    newPairs = placeStudents(student_list, chair_list)
+    print("print pairs")
+    for x in newPairs:
+        print(x)
+    # for chair in chair_list:
+    #     print(chair)
