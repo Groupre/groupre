dce16307cd1f42b8156eb7f7abd14fbdbd5aed73
kamisch <chengtwv2@gmail.com>
2019-01-15 16:42:48 -0500

    rewrote algo

--- src/data_structures/chair.py
+++ src/data_structures/chair.py
@@ -8,17 +8,23 @@ class Chair:
     aisle: bool = False
     is_broken: bool = False
     student_id: int = None
+    num_points : int = 0
+    taken :bool = False
 
     def __init__(self, chair_id, preferences):
         self.chair_id = chair_id
         if "front" in preferences:
             self.front = True
+            self.num_points += 1
         if "back" in preferences:
             self.back = True
+            self.num_points += 30
         if "left" in preferences:
             self.left = True
+            self.num_points += 20
         if "aisle" in preferences:
             self.aisle = True
+            self.num_points += 40
         if "broken" in preferences:
             self.is_broken = True
 
--- src/data_structures/student.py
+++ src/data_structures/student.py
@@ -8,26 +8,33 @@ class Student:
     pref_back: bool = False
     pref_left: bool = False
     pref_aisle: bool = False
+    num_points: int = 0
+    taken :bool = False
+
 
     def __init__(self, student_id, is_vip, preferences):
         self.student_id = student_id
         if is_vip == "True":
             self.is_VIP = True
+            self.num_points += 100
         else:
             self.is_VIP = False
         if "f" in preferences:
             self.pref_front = True
             self.has_pref = True
+            self.num_points += 1
         if "b" in preferences:
             self.pref_back = True
             self.has_pref = True
+            self.num_points += 30
         if "l" in preferences:
             self.pref_left = True
             self.has_pref = True
+            self.num_points += 20
         if "a" in preferences:
             self.pref_aisle = True
             self.has_pref = True
-
+            self.num_points += 40
     def __str__(self):
         return ("Student ID: " + self.student_id + " VIP: " + str(self.is_VIP) + " Front: " + str(self.pref_front) +
                 " Back: " + str(self.pref_back) + " Left: " + str(self.pref_left) + " Aisle: " + str(self.pref_aisle))
